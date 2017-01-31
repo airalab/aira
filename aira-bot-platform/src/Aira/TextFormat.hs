d1133f0b08f57664d4b16e4c2b1533ff831fc6d3
Alexander Krupenkin <mail@akru.me>
2017-01-31 16:33:14 +0300

    Switch to etherscan mainnet helper links

--- aira-bot-platform/src/Aira/TextFormat.hs
+++ aira-bot-platform/src/Aira/TextFormat.hs
@@ -22,13 +22,13 @@ import Data.Monoid ((<>))
 import Data.Text
 
 uri_tx :: Text -> Text
---uri_tx tx = "[" <> tx <> "](https://etherscan.io/tx/" <> tx <> ")"
-uri_tx tx = "[" <> tx <> "](https://testnet.etherscan.io/tx/" <> tx <> ")"
+uri_tx tx = "[" <> tx <> "](https://etherscan.io/tx/" <> tx <> ")"
+--uri_tx tx = "[" <> tx <> "](https://testnet.etherscan.io/tx/" <> tx <> ")"
 
 uri_address :: Address -> Text
---uri_address a = "[" <> toText a <> "](https://etherscan.io/address/" <> toText a <> ")"
-uri_address a = "[" <> toText a <> "](https://testnet.etherscan.io/address/" <> toText a <> ")"
+uri_address a = "[" <> toText a <> "](https://etherscan.io/address/" <> toText a <> ")"
+--uri_address a = "[" <> toText a <> "](https://testnet.etherscan.io/address/" <> toText a <> ")"
 
 uri_block :: Integer -> Text
---uri_block b = "[" <> pack (show b) <> "](https://etherscan.io/block/" <> pack (show b) <> ")"
-uri_block b = "[" <> pack (show b) <> "](https://testnet.etherscan.io/block/" <> pack (show b) <> ")"
+uri_block b = "[" <> pack (show b) <> "](https://etherscan.io/block/" <> pack (show b) <> ")"
+--uri_block b = "[" <> pack (show b) <> "](https://testnet.etherscan.io/block/" <> pack (show b) <> ")"
