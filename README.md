7dcbb7f2ab4efae15557def9433502180f1a1352
Alexander Krupenkin <mail@akru.me>
2017-05-02 12:41:35 +0300

    Added installation script

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # Aira
 
 Please, start here: https://github.com/airalab/aira/releases
+
+### Installation
+
+**Stable** by IPFS:
+
+```bash
+curl -s http://ipfs.io/ipfs/QmUWi93qFRN6f35NQkDKaWJnokKCcrMYmtL9bponMNaBwm | bash
+```
+
+Or **latest** by GitHub:
+
+```bash
+curl -sSL https://raw.githubusercontent.com/airalab/aira/master/tool/start.sh | bash
+```
---
+++ tool/start.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# AIRA image installation script
+#
+
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
+echo "AIRA :: Install Docker..."
+curl -sSL https://get.docker.com | sh
+
+echo "AIRA :: Install Docker Compose..."
+curl -L --fail https://github.com/docker/compose/releases/download/1.12.0/run.sh > /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+echo "AIRA :: Building image..."
+git clone --recursive https://github.com/airalab/aira
+cd aira/aira-IoT/docker/shared-state-liability
+docker-compose build
+
+echo "AIRA :: Running..."
+docker-compose up -d
